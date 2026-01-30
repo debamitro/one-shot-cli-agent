1de6285a108c6e7cbacf6f44a5dad1666194203d
Debamitro Chakraborti <debamitro@gmail.com>
2026-01-29 22:21:09 -0500

    Added linux bundling command

---
+++ scripts/bundle_linux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+ver=0.3
+
+cargo zigbuild --target x86_64-unknown-linux-gnu --release
+cd target/x86_64-unknown-linux-gnu/release
+zip ../../../codeagent_${ver}_linux.zip codeagent
